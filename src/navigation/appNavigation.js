9d750c2580ee27803cb914df2d371da55eeb7596
CPPtrueCoder <andierodin@gmail.com>
2020-06-29 15:36:20 +0300

    Refactoring code

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -1,24 +1,77 @@
+import React from "react";
 import {createAppContainer} from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
 import {MainScreen} from "../screens/MainScreen";
 import {PostScreen} from "../screens/PostScreen";
 import {Platform} from 'react-native'
 import {THEME} from "../theme";
+import {Ionicons} from '@expo/vector-icons'
+import {BookedScreen} from "../screens/BookedScreen";
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs'
+import {Post} from "../components/Post";
+
+const navigatorOptions ={
+	defaultNavigationOptions: {
+		headerStyle: {
+			backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff'
+			
+		},
+		headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR
+	}
+};
+
 
 
 const PostNavigator = createStackNavigator ({
 	Main: MainScreen,
+	Post: PostScreen
+}, navigatorOptions
+);
+
+const BookedNavigator = createStackNavigator ({
+		Booked: BookedScreen,
+		Post: PostScreen
+	},
+	navigatorOptions
+	
+);
+
+const bottomTabsConfig = {
 	Post: {
-	screen: PostScreen
-	}},{
-initialRouteName: 'Main',
-	defaultNavigationOptions:{
-	headerStyle:{
-		backgroundColor: Platform.OS==='android' ? THEME.MAIN_COLOR : '#fff'
+		screen: PostNavigator,
+		navigationOptions: {
+			tabBarLabel:'Все',
+			tabBarIcon: info => <Ionicons name='ios-albums'
+			                              size={25}
+			                              color={info.tintColor}/>
+		}
 		
 	},
-		headerTintColor:Platform.OS ==='android'?'#fff' :THEME.MAIN_COLOR
-	}
-});
+	Booked: {
+		screen: BookedNavigator,
+		navigationOptions: {
+			tabBarLabel:'Избранное',
+			tabBarIcon: info => <Ionicons name='ios-star'
+			                              size={25}
+			                              color={info.tintColor}/>
+		}
+	},
+	
+};
 
-export const AppNavigation = createAppContainer(PostNavigator);
+const BottomNavigator =Platform.OS ==='android' ? createMaterialBottomTabNavigator(bottomTabsConfig,{
+	activeTintColor:'#fff',
+	shifting:true,
+	barStyle:{
+		backgroundColor:THEME.MAIN_COLOR
+	}
+}) : createBottomTabNavigator (
+	bottomTabsConfig,
+	{
+		tabBarOptions:{
+		activeTintColor:THEME.MAIN_COLOR
+		}
+	});
+export const AppNavigation = createAppContainer (BottomNavigator);
+
--- src/screens/BookedScreen.js
+++ src/screens/BookedScreen.js
@@ -1,31 +1,26 @@
 import React from 'react'
-
 import {DATA} from "../data";
 import {Post} from "../components/Post";
 import {HeaderButtons, Item} from 'react-navigation-header-buttons'
 import {AppHeaderIcon} from "../components/AppHeaderIcon";
 import {PostList} from "../components/PostList";
 
-export const MainScreen = ({navigation}) => {
+export const BookedScreen = ({navigation}) => {
 	
 	const openPostHandler = post => {
 		navigation.navigate ('Post', {
-			postId: post.id, date: post.date,booked:post.booked
+			postId: post.id, date: post.date, booked: post.booked
 		});
 	};
 	
 	return (
-		<PostList data={DATA} onOpen={openPostHandler}/>)
+		<PostList data={DATA.filter (post => post.booked)}
+		          onOpen={openPostHandler}/>)
 };
 
-MainScreen.navigationOptions = {
-	headerTitle: 'I HATE IT',
-	//Need update for future versions from object to callback function with return JSX
-	headerRight: (<HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
-		<Item title='Take photo'
-		      iconName='ios-camera'
-		      onPress={() => console.log ('Press')}/>
-	</HeaderButtons>),
+BookedScreen.navigationOptions = {
+	headerTitle: 'Liked',
+	
 	headerLeft: <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
 		<Item title='Toogle Drawer'
 		      iconName='ios-menu'
